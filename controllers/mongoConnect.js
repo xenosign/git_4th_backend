ca7b060b61341b082f250541ad8eb946352f0c12
kdtTetz <xenosign@naver.com>
2022-11-30 17:50:51 +0900

    20221130 수업 코드 추가

--- controllers/mongoConnect.js
+++ controllers/mongoConnect.js
@@ -1,13 +1,35 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri =
-  'mongodb+srv://tetz:dlrladk@cluster0.sdiakr0.mongodb.net/?retryWrites=true&w=majority';
+  'mongodb+srv://tetz:qwer1234@cluster0.sdiakr0.mongodb.net/?retryWrites=true&w=majority';
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db('test').collection('devices');
-  // perform actions on the collection object
-  client.close();
-});
+
+async function dbConnect() {
+  await client.connect();
+  const users = client.db('kdt4').collection('users');
+  await users.deleteMany({});
+  await users.insertMany([
+    {
+      name: 'pororo',
+      age: 5,
+    },
+    {
+      name: 'loopy',
+      age: 6,
+    },
+    {
+      name: 'crong',
+      age: 4,
+    },
+  ]);
+  const data = users.find({});
+  await data.forEach(console.log);
+  await client.close();
+}
+
+dbConnect();
