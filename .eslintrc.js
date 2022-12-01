3914e0c52cee9a94c44aee5227a4cd9afbc96721
kdtTetz <xenosign@naver.com>
2022-12-01 10:44:50 +0900

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
+/*
+
+npm install --save-dev prettier
+npm install --save-dev eslint-config-airbnb-base eslint-plugin-import
+npm install --save-dev typescript
+
+*/
+
 module.exports = {
   extends: ['airbnb-base'],
   rules: {
